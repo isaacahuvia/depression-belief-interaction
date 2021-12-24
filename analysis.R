91c44e844f9227e40233b6830a778da790462e5a
Laura Jans <you@example.com>
2021-12-24 00:57:30 -0500

    Preparing data, regressions, plots

--- analysis.R
+++ analysis.R
@@ -1,53 +1,66 @@
 ###################
-##  Correct Sample Code  ##
+##  12/23/21  ##
 ###################
 
 ####  Startup  ####
-## Load packages
 
-#test comment 12/20
+# Load packages
 
 if(!require(tidyverse)){install.packages('tidyverse')}
 if(!require(assertr)){install.packages('assertr')}
 if(!require(here)){install.packages('here')}
+if(!require(ggplot2)){install.packages('ggplot2')}
 
 library(tidyverse)
 library(assertr)
 library(here)
+library(ggplot2)
 
-####  Load Data  ####
+#### Load data ####
+
 # The here() package returns filepaths based on the arguments you give it,
 # starting from the folder with your .Rproj file; in this case, it will return
 # the filepath to the dataset inside the "data" folder
 # This is especially useful when a script will be run on Windows and Mac 
 # computers, as they use different folder separators ("/" vs "\")
 
-df <- readRDS(here("data", "T2T Data for Jans et al.rds"))
-str(df)
-
-#comment
+df_1 <- readRDS(here("data", "T2T Data for Jans et al.rds"))
 
 ####  Prepare Data  ####
-df <- df %>%
-  
-  # Create new variables
-  mutate(race_recoded = "...") %>%
-  
-  # Confirm no missing values
-  verify(!is.na(yb_cdi_1)) %>% #see vignette("assertr")
-  
-  # Select only variables needed for analysis
-  select(everything())
 
+  # Adding a column for CDI mean
 
+  df_2 <- df_1 %>% mutate(cdi_mean = rowMeans(select(.,contains("cdi")), na.rm = FALSE))
+  
+  # Selecting variables
+  
+  df_3 <- df_2 %>% select(yb_lsmh_id, yb_permanence, yb_cause_brain, yb_cause_env, yb_change_brain, yb_change_env, cdi_mean)
+  
+  # Removing rows containing NA
+  
+  df_4 <- df_3 %>% filter(complete.cases(.))
+  
 
 ####  Analysis  ####
-## Regression
-my_model <- lm(data = df, yb_cdi_1 ~ yb_cdi_2 + yb_cdi_3)
-summary(my_model)
 
+## Regression with CDI
+
+my_model_1 <- lm(data = df_4, yb_permanence ~ yb_cause_brain + yb_change_brain + yb_cause_brain*yb_change_brain + cdi_mean)
+summary(my_model_1)
+
+my_model_2 <- lm(data = df_4, yb_permanence ~ yb_cause_env + yb_change_env + yb_cause_env*yb_change_env + cdi_mean)
+summary(my_model_2)
+
+## Regression without CDI
+
+my_model_3 <- lm(data = df_4, yb_permanence ~ yb_cause_brain + yb_change_brain + yb_cause_brain*yb_change_brain)
+summary(my_model_3)
+
+my_model_4 <- lm(data = df_4, yb_permanence ~ yb_cause_env + yb_change_env + yb_cause_env*yb_change_env)
+summary(my_model_4)
 
 ## Plots
-df %>%
-  ggplot() +
-  geom_histogram(aes(yb_cdi_1))
+
+interaction.plot(x.factor=df_4$yb_cause_brain, trace.factor=df_4$yb_change_brain, response=df_4$yb_permanence)
+
+interaction.plot(x.factor=df_4$yb_cause_env, trace.factor=df_4$yb_change_env, response=df_4$yb_permanence)
