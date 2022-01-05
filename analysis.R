e5773a5a42c3b7e09841e1f57876baeb18339ee3
Isaac Ahuvia <isaacahuvia@gmail.com>
2022-01-04 21:52:25 -0500

    isaac's edits initial commit

--- analysis.R
+++ analysis.R
@@ -1,45 +1,33 @@
-###################
-##  12/27/21  ##
-###################
+#####################################
+##  Depression-Belief Interaction  ##
+#####################################
 
 ####  Startup  ####
-
 # Load packages
-
-if(!require(tidyverse)){install.packages('tidyverse')}
-if(!require(here)){install.packages('here')}
-if(!require(ggplot2)){install.packages('ggplot2')}
-
 library(tidyverse)
 library(here)
 library(ggplot2)
 
 
+
 #### Load data ####
-
-# The here() package returns filepaths based on the arguments you give it,
-# starting from the folder with your .Rproj file; in this case, it will return
-# the filepath to the dataset inside the "data" folder
-# This is especially useful when a script will be run on Windows and Mac 
-# computers, as they use different folder separators ("/" vs "\")
-
 df_1 <- readRDS(here("data", "T2T Data for Jans et al.rds"))
 
 
+
 ####  Prepare Data  ####
-
+df_2 <- df_1 %>%
+  
   # Adding a column for CDI mean
-
-  df_2 <- df_1 %>% mutate(cdi_mean = rowMeans(select(.,contains("cdi")), na.rm = FALSE))
+  mutate(cdi_mean = rowMeans(select(.,contains("cdi")), na.rm = FALSE)) %>%
   
   # Selecting variables
-  
-  df_3 <- df_2 %>% select(yb_lsmh_id, yb_permanence, yb_cause_brain, yb_cause_env, yb_change_brain, yb_change_env, cdi_mean)
+  select(yb_lsmh_id, yb_permanence, yb_cause_brain, yb_cause_env, yb_change_brain, yb_change_env, cdi_mean) %>%
   
   # Removing rows containing NA
-  
-  df_4 <- df_3 %>% filter(complete.cases(.))
-  
+  filter(complete.cases(.))
+
+
 
 ####  Analysis  ####
 
